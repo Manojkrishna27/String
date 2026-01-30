319db091c2cd5097c1393821c45a7718920964b4
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-01-30 22:13:10 +0530

    count_vowels and consonants

---
+++ count_vowles&constant.py
@@ -0,0 +1,12 @@
+def count_vowels_consonants(word):
+    word=word.lower() # this will convert A to a capital to lower
+    vowels=0
+    consonants=0
+    for ch in word:
+        if ch in "aeiou":  # checking ch in vowels
+            vowels+=1
+        elif ch.isalpha(): # this ensure that we will only count the actual letters
+            consonants+=1
+    return "the vowels in a string:",vowels,"the consonants in a string:",consonants
+word="apple1"
+print(count_vowels_consonants(word))
