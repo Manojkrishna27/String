b0d018e0a72ca562e843f4a25c63041d613c4781
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-02-04 12:29:58 +0530

    count_duplicate

--- count_dup.py
+++ count_dup.py
@@ -1,8 +1,8 @@
 name="programming"
 freq={}
 for ch in name:
-    freq[ch]=freq.get(ch,0)+1
+    freq[ch]=freq.get(ch,0)+1 # getting key and value
 
 for ch in freq:
-    if freq[ch]>1:
+    if freq[ch]>1: # checking value greater than 1
         print(ch,freq[ch])
